e6bd4da0a6f2b8ceb23c49cdb48358c09abd1900
Alex Hover <ahover307@gmail.com>
2020-02-17 15:33:34 -0500

    Working Version

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import NavBarComponent from './components/layout/NavBarComponent'
-// import MainScheduler from "./components/scheduler/MainScheduler";
-// import Calendar from './components/calendar/Calendar';
+import MainScheduler from "./components/scheduler/MainScheduler";
+import Calendar from './components/calendar/Calendar';
 import SplashScreen from './SplashScreen';
 
 import './App.css';
@@ -9,11 +10,16 @@ import './App.css';
 function App() {
     return (
         <div>
-            <NavBarComponent/>
-            <div className='App'>
-                <SplashScreen/>
+            <BrowserRouter>
+                <NavBarComponent/>
 
-            </div>
+                <Switch>
+                    <Route exact path='/'> <SplashScreen/> </Route>
+                    <Route path={'/scheduler'}> <MainScheduler/> </Route>
+                    <Route path={'/calendar'}> <Calendar/> </Route>
+                </Switch>
+
+            </BrowserRouter>
         </div>
     );
 }
