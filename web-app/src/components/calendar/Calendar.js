b576da5868424614b40398affc9c9775e195b326
Alex Hover <ahover307@gmail.com>
2020-02-20 09:26:50 -0500

    added firebase api keys, and began work on calendar

--- web-app/src/components/calendar/Calendar.js
+++ web-app/src/components/calendar/Calendar.js
@@ -1,20 +1,29 @@
 import React, {Component} from "react";
 import Calendar from "react-calendar";
+import MainScheduler from "../scheduler/MainScheduler";
 
 class calendar extends Component {
     state = {
         date: new Date(),
     };
 
-    onChange = date => this.setState({date});
+    onClickDay = date => {
+        <MainScheduler date={date}/>
+    };
 
+//https://www.npmjs.com/package/react-calendar
     render() {
         return (
             <div>
-                <Calendar
-                    onChange={this.onChange}
-                    value={this.state.date}
-                />
+                <div className={'center'}>
+                    <Calendar
+                        calendarType={"US"}
+                        onClickDay={this.onClickDay}
+                        value={this.state.date}
+                    />
+                    <p>In future versions, upon clicking on a date this should take you directly to the scheduler page
+                        for that date.</p>
+                </div>
             </div>
         )
     }
--- web-app/src/firebase.js
+++ web-app/src/firebase.js
@@ -1,13 +1,16 @@
 import firebase from 'firebase'
 
-var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
-    apiKey: "unreadablestuff",
-    authDomain: "your-domain-name.firebaseapp.com",
-    databaseURL: "https://your-domain-name.firebaseio.com",
-    storageBucket: "your-domain-name.appspot.com",
-    messagingSenderId: "123123123123"
+const firebaseConfig = {
+    apiKey: "AIzaSyBIyEdACmURTID4gh9KsJyJcbi0tCc8h-s",
+    authDomain: "paramount-group-scheduler.firebaseapp.com",
+    databaseURL: "https://paramount-group-scheduler.firebaseio.com",
+    projectId: "paramount-group-scheduler",
+    storageBucket: "paramount-group-scheduler.appspot.com",
+    messagingSenderId: "187693486731",
+    appId: "1:187693486731:web:33d2ac8e49733d6b55ba8e",
+    measurementId: "G-J2VK2JJSJ4"
 };
 
-var fire = firebase.initializeApp(config);
+var fire = firebase.initializeApp(firebaseConfig);
 
 export default fire;
