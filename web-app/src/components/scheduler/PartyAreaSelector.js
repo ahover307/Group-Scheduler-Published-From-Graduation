8a439275079eb3d64293c96cc2125699529b67c6
Alex H <ahover307@gmail.com>
2020-02-22 14:00:17 -0500

    Added state to selectors

--- web-app/src/components/scheduler/PartyAreaSelector.js
+++ web-app/src/components/scheduler/PartyAreaSelector.js
@@ -1,36 +1,32 @@
 import React, {Component} from "react";
 
-// import M from 'materialize-css'
-
 class PartyAreaSelector extends Component {
     state = {
         ninjaParty: 'checked',
-        partyAreas: [],
+        partyArea1: -1,
+        partyArea2: -1,
+        partyArea3: -1,
         areasNeeded: 2
     };
 
 
     handleChange = (e) => {
-        let tempArr = this.state.partyAreas;
-
-        if (e.target.value()) {
-            tempArr.splice(tempArr.indexOf(e.target.id));
-            this.setState({partyAreas: tempArr});
-        } else {
-            tempArr.push(e.target.id);
-            this.setState({partyAreas: tempArr});
+        if ('first' === e.target.name) {
+            this.setState({partyArea1: e.target.value});
         }
-        console.log(e.target.value);
+
+        if ('second' === e.target.name) {
+            this.setState({partyArea2: e.target.value});
+        }
+
+        if ('third' === e.target.name) {
+            this.setState({partyArea3: e.target.value});
+        }
     };
 
     handleSubmit = (e) => {
         console.log('Submit submitted');
     };
-
-    // componentDidMount() {
-    //     M.AutoInit();
-    //     console.log('Auto init ran')
-    // }
 
     render() {
         const partyTypes = [
@@ -52,24 +48,24 @@ class PartyAreaSelector extends Component {
                 <div className={'container'}>
                     <form action={'#'} onSubmit={this.handleSubmit}>
                         <div className={'input-field'}>
-                            <select className={'browser-default'} defaultValue={''}
-                                    disabled={this.state.areasNeeded < 1}>
+                            <select name={'first'} className={'browser-default'} defaultValue={''}
+                                    disabled={this.state.areasNeeded < 1} onChange={this.handleChange}>
                                 <option value={''} disabled={true}>Choose First Party Area</option>
                                 {optionsList}
                             </select>
                         </div>
 
                         <div className="input-field">
-                            <select className={'browser-default'} defaultValue={''}
-                                    disabled={this.state.areasNeeded < 2}>
+                            <select name={'second'} className={'browser-default'} defaultValue={''}
+                                    disabled={this.state.areasNeeded < 2} onChange={this.handleChange}>
                                 <option value={''} disabled>Choose Second Party Area</option>
                                 {optionsList}
                             </select>
                         </div>
 
                         <div className="input-field">
-                            <select className={'browser-default'} defaultValue={''}
-                                    disabled={this.state.areasNeeded < 3}>
+                            <select name={'third'} className={'browser-default'} defaultValue={''}
+                                    disabled={this.state.areasNeeded < 3} onChange={this.handleChange}>
                                 <option value={''} disabled>Choose Third Party Area</option>
                                 {optionsList}
                             </select>
