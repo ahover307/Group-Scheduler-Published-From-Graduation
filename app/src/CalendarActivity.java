fe8d8af18eee24d5a1326dcc1a20b6b019517089
nguyenryan31 <50286138+nguyenryan31@users.noreply.github.com>
2020-03-01 21:07:42 -0500

    Update CalendarActivity.java

--- app/src/CalendarActivity.java
+++ app/src/CalendarActivity.java
@@ -2,11 +2,13 @@ package edu.psu.jbr5410.paramount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
 
 
 import java.util.Date;
@@ -23,10 +25,18 @@ public class CalendarActivity extends AppCompatActivity {
     }
 
     public void Button(View view) {
+
         Intent intent = new Intent(this, PackageActivity.class);
-        CalendarView calendar = findViewById(R.id.calendarView);
-        Long date = calendar.getDate();
-        intent.putExtra("date", date);
+        DatePicker calendar = findViewById(R.id.datePicker1);
+        int day = calendar.getDayOfMonth();
+        int month = calendar.getMonth();
+        int year = calendar.getYear();
+
+        intent.putExtra("day", day);
+        intent.putExtra("month", month);
+        intent.putExtra("year", year);
         startActivity(intent);
     }
+
+
 }
