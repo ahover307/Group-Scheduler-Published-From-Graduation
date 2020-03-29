bbb532114dd7dca5955539b08822d1e4d3965f4d
nguyenryan31 <50286138+nguyenryan31@users.noreply.github.com>
2020-03-29 16:22:41 -0400

    Split date into day, month, year and day of week for algorithm compatibility

--- app/src/ThreeRoomActivity.java
+++ app/src/ThreeRoomActivity.java
@@ -9,8 +9,8 @@ import android.widget.Spinner;
 
 public class ThreeRoomActivity extends AppCompatActivity {
 
-    public String date;
-    public String partyPackage;
+    private int day, month, year, dayOfWeek;
+    private String partyPackage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +18,15 @@ public class ThreeRoomActivity extends AppCompatActivity {
         setContentView(R.layout.activity_three_room);
 
         Intent intent = getIntent();
-        String date = intent.getStringExtra("date");
-        String partyPackage = intent.getStringExtra("package");
+        day = intent.getIntExtra("day", -1);
+        month = intent.getIntExtra("month", -1);
+        year = intent.getIntExtra("year", -1);
+        dayOfWeek = intent.getIntExtra("dayOfWeek",-1);
+        partyPackage = intent.getStringExtra("package");
     }
 
     public void submit(View view) {
-        Intent intent = new Intent(this, FinalDetailsActivity.class);
+        Intent intent = new Intent(this, TimeslotActivity.class);
 
         Spinner roomSpinner = findViewById(R.id.spinner_rooms);
         String room = roomSpinner.getSelectedItem().toString();
@@ -34,7 +37,10 @@ public class ThreeRoomActivity extends AppCompatActivity {
 
         String[] rooms = {room, room2, room3};
 
-        intent.putExtra("date", date);
+        intent.putExtra("day", day);
+        intent.putExtra("month", month);
+        intent.putExtra("year", year);
+        intent.putExtra("dayOfWeek", dayOfWeek);
         intent.putExtra("package", partyPackage);
         intent.putExtra("rooms", rooms);
 
