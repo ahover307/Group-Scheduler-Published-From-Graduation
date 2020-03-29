20a06eaff7f7bea08daa96dd12a21498163dd030
nguyenryan31 <50286138+nguyenryan31@users.noreply.github.com>
2020-03-29 16:21:25 -0400

    Split date into day, month, year and day of week for algorithm compatibility

--- app/src/RoomActivity.java
+++ app/src/RoomActivity.java
@@ -9,8 +9,8 @@ import android.widget.Spinner;
 
 public class RoomActivity extends AppCompatActivity {
 
-    public String date;
-    public String partyPackage;
+    private int day, month, year, dayOfWeek;
+    private String partyPackage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +18,25 @@ public class RoomActivity extends AppCompatActivity {
         setContentView(R.layout.activity_room);
 
         Intent intent = getIntent();
-        date = intent.getStringExtra("date");
+        day = intent.getIntExtra("day", -1);
+        month = intent.getIntExtra("month", -1);
+        year = intent.getIntExtra("year", -1);
+        dayOfWeek = intent.getIntExtra("dayOfWeek",-1);
         partyPackage = intent.getStringExtra("package");
 
     }
 
     public void submit(View view) {
-        Intent intent = new Intent(this, FinalDetailsActivity.class);
+        Intent intent = new Intent(this, TimeslotActivity.class);
 
         Spinner roomSpinner = findViewById(R.id.spinner_rooms);
         String room = roomSpinner.getSelectedItem().toString();
         String[] rooms = {room};
 
-        intent.putExtra("date", date);
+        intent.putExtra("day", day);
+        intent.putExtra("month", month);
+        intent.putExtra("year", year);
+        intent.putExtra("dayOfWeek", dayOfWeek);
         intent.putExtra("package", partyPackage);
         intent.putExtra("rooms", rooms);
 
