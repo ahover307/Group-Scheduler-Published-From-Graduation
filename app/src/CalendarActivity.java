15d588a743eec6cb45abfd612c5ec98b5c0b891c
nguyenryan31 <50286138+nguyenryan31@users.noreply.github.com>
2020-03-29 16:18:41 -0400

    Split date into day, month, year and day of week for algorithm compatibility

--- app/src/CalendarActivity.java
+++ app/src/CalendarActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.icu.util.Calendar;
+import android.icu.util.LocaleData;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
+import android.widget.TextView;
 
+import java.time.*;
+import java.time.DayOfWeek;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 
@@ -29,12 +34,23 @@ public class CalendarActivity extends AppCompatActivity {
         Intent intent = new Intent(this, PackageActivity.class);
         DatePicker calendar = findViewById(R.id.datePicker1);
         int day = calendar.getDayOfMonth();
-        int month = calendar.getMonth();
+        int month = calendar.getMonth() + 1; // Months in DatePicker are 0-based
         int year = calendar.getYear();
+
+        LocalDate date = LocalDate.of(year, month, day);
+        DayOfWeek dayOfWeek = DayOfWeek.from(date);
+
+        int dow = dayOfWeek.getValue();
+
+        //TextView test = findViewById(R.id.text_calendarInstructs);
+        //test.setText(dow);
+
 
         intent.putExtra("day", day);
         intent.putExtra("month", month);
         intent.putExtra("year", year);
+
+        intent.putExtra("dayOfWeek", dow);
         startActivity(intent);
     }
 
