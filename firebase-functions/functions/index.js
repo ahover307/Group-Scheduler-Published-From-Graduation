b3740a796c9abdd093b980c5f214fd7da78ee974
JJ <jjarj83@gmail.com>
2020-03-30 12:50:53 -0400

    Updated function for email use and custom data

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -11,9 +11,10 @@ exports.paymentIntent = functions.https.onCall(async(data, context) => {
     const stripe = require('stripe')('sk_test_v9k8fKhH1oq3R0EnF2vg8n7M00zReeGEZs');
 
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: 1099,
+        amount: data.price,
         currency: 'usd',
         payment_method_types: ['card'],
+        receipt_email: data.email,
     });
 
     const clientSecret = paymentIntent.client_secret;
