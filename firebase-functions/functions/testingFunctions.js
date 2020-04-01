e4ec22c6671527ff1ef988fe3957a3b7aa536060
Alex Hover <ahover307@gmail.com>
2020-04-01 12:53:28 -0400

    Working on creating index

--- firebase-functions/functions/testingFunctions.js
+++ firebase-functions/functions/testingFunctions.js
@@ -23,21 +23,35 @@ exports.fillOpenHours = functions.https.onCall((data, context) => {
 });
 
 exports.pullOpenHours = functions.https.onCall(async (data, context) => {
-    let day = 1;
-    let openHours = db.collection('OpenHours').where('room', '==', day);
-    openHours = openHours.where('dayOfWeek', '==', day);
-    let test = await openHours.get()
-        .then(snapshot => {
-            let temp = [];
-            snapshot.forEach(doc => {
-                temp.push(doc.data().start);
-                temp.push(doc.data().end);
-            });
-            return temp;
-        })
-        .catch(err => {
-            console.log('Error getting documents', err);
-        });
+    // let day = 1;
+    // let openHours = db.collection('OpenHours').where('room', '==', day);
+    // openHours = openHours.where('dayOfWeek', '==', day);
+    // let test = await openHours.get()
+    //     .then(snapshot => {
+    //         let temp = [];
+    //         snapshot.forEach(doc => {
+    //             temp.push(doc.data().start);
+    //             temp.push(doc.data().end);
+    //         });
+    //         return temp;
+    //     })
+    //     .catch(err => {
+    //         console.log('Error getting documents', err);
+    //     });
+    //
+    // return test;
 
-    return test;
+    let partiesRef = db.collection('Parties').where('dateDay', '==', partyDateDay).where('dateMonth', '==', partyDateMonth).where('dateYear', '==', partyDateYear);
+    partiesRef = partiesRef.where('roomsRequested', 'array-contains', roomsRequested[0]);
+    let filledTimes = await partiesRef.get().then((snapshot) => {
+        let temp = [];
+        snapshot.forEach(doc => {
+            let index = doc.data().roomsRequested.getIndex(roomsRequested[0]);
+            temp.push(doc.data().roomTimes[index]);
+            temp.push(doc.data().roomTimes[index + 1]);
+        });
+        return temp;
+    }).catch((err) => {
+        throw new functions.https.HttpsError('parties-reference-break', 'Failed looking for the previous party times: ' + err);
+    });
 });
