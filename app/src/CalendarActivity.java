0e884cdcb3ef3b1d22a47f84df1388a5ec7a5cd1
nguyenryan31 <50286138+nguyenryan31@users.noreply.github.com>
2020-04-05 22:56:59 -0400

    Update CalendarActivity.java

--- app/src/CalendarActivity.java
+++ app/src/CalendarActivity.java
@@ -1,58 +1,265 @@
 package edu.psu.jbr5410.paramount;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
-import android.content.Intent;
-import android.icu.util.Calendar;
-import android.icu.util.LocaleData;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.CalendarView;
-import android.widget.DatePicker;
-import android.widget.TextView;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.FirebaseFunctionsException;
+import com.google.firebase.functions.HttpsCallableResult;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.stripe.android.ApiResultCallback;
+import com.stripe.android.PaymentConfiguration;
+import com.stripe.android.PaymentIntentResult;
+import com.stripe.android.Stripe;
+import com.stripe.android.model.ConfirmPaymentIntentParams;
+import com.stripe.android.model.PaymentIntent;
+import com.stripe.android.model.PaymentMethodCreateParams;
+import com.stripe.android.view.CardInputWidget;
 
-import java.time.*;
-import java.time.DayOfWeek;
+import org.json.JSONException;
 
-import java.time.LocalDate;
-import java.util.Date;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
+public class CheckoutActivity extends AppCompatActivity {
 
-public class CalendarActivity extends AppCompatActivity {
-
+    private String paymentIntentClientSecret;
+    private Stripe stripe;
+    private FirebaseFunctions mFunctions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.activity_checkout);
+        mFunctions = FirebaseFunctions.getInstance();
+
+
+        startCheckout();
     }
 
-    public void Button(View view) {
+    private void startCheckout() {
+        String email = "nguyenryan31@gmail.com";
+        int price = 2099;
+        paymentIntent(email, price)
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Exception e = task.getException();
+                            if (e instanceof FirebaseFunctionsException) {
+                                FirebaseFunctionsException ffe = (FirebaseFunctionsException) e;
+                                FirebaseFunctionsException.Code code = ffe.getCode();
+                                Object details = ffe.getDetails();
+                            }
 
-        Intent intent = new Intent(this, PackageActivity.class);
-        DatePicker calendar = findViewById(R.id.datePicker1);
-        int day = calendar.getDayOfMonth();
-        int month = calendar.getMonth() + 1; // Months in DatePicker are 0-based
-        int year = calendar.getYear();
-
-        LocalDate date = LocalDate.of(year, month, day);
-        DayOfWeek dayOfWeek = DayOfWeek.from(date);
-
-        int dow = dayOfWeek.getValue();
-
-        //TextView test = findViewById(R.id.text_calendarInstructs);
-        //test.setText(dow);
+                        }
+                        paymentIntentClientSecret = task.getResult();
+                    }
+                });
 
 
-        intent.putExtra("day", day);
-        intent.putExtra("month", month);
-        intent.putExtra("year", year);
+        Button payButton = findViewById(R.id.payButton);
+        payButton.setOnClickListener((View view) -> {
+            CardInputWidget cardInputWidget = findViewById(R.id.cardInputWidget);
+            PaymentMethodCreateParams params = cardInputWidget.getPaymentMethodCreateParams();
+            if (params != null) {
+                ConfirmPaymentIntentParams confirmParams = ConfirmPaymentIntentParams
+                        .createWithPaymentMethodCreateParams(params, paymentIntentClientSecret);
 
-        intent.putExtra("dayOfWeek", dow);
-        startActivity(intent);
+                stripe = new Stripe(getApplicationContext(),
+                        "pk_test_rKltl8cKNz9NLrOL7w1KT22800Yi2Zh7n9"
+                );
+                stripe.confirmPayment(this, confirmParams);
+            }
+        });
+
+
     }
 
+    private void displayAlert(@NonNull String title,
+                              @Nullable String message,
+                              boolean restartDemo) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message);
+        if (restartDemo) {
+            builder.setPositiveButton("Restart demo",
+                    (DialogInterface dialog, int index) -> {
+                        CardInputWidget cardInputWidget = findViewById(R.id.cardInputWidget);
+                        cardInputWidget.clear();
+                        startCheckout();
+                    });
+        } else {
+            builder.setPositiveButton("Ok", null);
+        }
+        builder.create().show();
+    }
+
+    //paymentIntent
+    private Task<String> paymentIntent(String email, int price) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("email", email);
+        data.put("price", price);
+
+        return mFunctions
+                .getHttpsCallable("paymentIntent")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, String>() {
+                    @Override
+                    public String then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        String result = (String) task.getResult().getData();
+                        return result;
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Handle the result of stripe.confirmPayment
+        stripe.onPaymentResult(requestCode, data, new PaymentResultCallback(this));
+    }
+
+
+    private void onPaymentSuccess(@NonNull final Response response) throws IOException {
+        Gson gson = new Gson();
+        Type type = new TypeToken<Map<String, String>>(){}.getType();
+        Map<String, String> responseMap = gson.fromJson(
+                Objects.requireNonNull(response.body()).string(),
+                type
+        );
+
+        // The response from the server includes the Stripe publishable key and
+        // PaymentIntent details.
+        // For added security, our sample app gets the publishable key from the server
+        String stripePublishableKey = responseMap.get("publishableKey");
+        paymentIntentClientSecret = responseMap.get("clientSecret");
+
+        // Configure the SDK with your Stripe publishable key so that it can make requests to the Stripe API
+        stripe = new Stripe(
+                getApplicationContext(),
+                Objects.requireNonNull(stripePublishableKey)
+        );
+    }
+
+    private static final class PayCallback implements Callback {
+        @NonNull private final WeakReference<CheckoutActivity> activityRef;
+
+        PayCallback(@NonNull CheckoutActivity activity) {
+            activityRef = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void onFailure(@NonNull Call call, @NonNull IOException e) {
+            final CheckoutActivity activity = activityRef.get();
+            if (activity == null) {
+                return;
+            }
+
+            activity.runOnUiThread(() ->
+                    Toast.makeText(
+                            activity, "Error: " + e.toString(), Toast.LENGTH_LONG
+                    ).show()
+            );
+        }
+
+        @Override
+        public void onResponse(@NonNull Call call, @NonNull final Response response)
+                throws IOException {
+            final CheckoutActivity activity = activityRef.get();
+            if (activity == null) {
+                return;
+            }
+
+            if (!response.isSuccessful()) {
+                activity.runOnUiThread(() ->
+                        Toast.makeText(
+                                activity, "Error: " + response.toString(), Toast.LENGTH_LONG
+                        ).show()
+                );
+            } else {
+                activity.onPaymentSuccess(response);
+            }
+        }
+    }
+
+    private static final class PaymentResultCallback
+            implements ApiResultCallback<PaymentIntentResult> {
+        @NonNull private final WeakReference<CheckoutActivity> activityRef;
+
+        PaymentResultCallback(@NonNull CheckoutActivity activity) {
+            activityRef = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void onSuccess(@NonNull PaymentIntentResult result) {
+            final CheckoutActivity activity = activityRef.get();
+            if (activity == null) {
+                return;
+            }
+
+            PaymentIntent paymentIntent = result.getIntent();
+            PaymentIntent.Status status = paymentIntent.getStatus();
+            if (status == PaymentIntent.Status.Succeeded) {
+                // Payment completed successfully
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                activity.displayAlert(
+                        "Payment completed",
+                        gson.toJson(paymentIntent),
+                        true
+                );
+            } else if (status == PaymentIntent.Status.RequiresPaymentMethod) {
+                // Payment failed – allow retrying using a different payment method
+                activity.displayAlert(
+                        "Payment failed",
+                        Objects.requireNonNull(paymentIntent.getLastPaymentError()).getMessage(),
+                        false
+                );
+            }
+        }
+
+        @Override
+        public void onError(@NonNull Exception e) {
+            final CheckoutActivity activity = activityRef.get();
+            if (activity == null) {
+                return;
+            }
+
+            // Payment request failed – allow retrying using the same payment method
+            activity.displayAlert("Error", e.toString(), false);
+        }
+    }
 
 }
