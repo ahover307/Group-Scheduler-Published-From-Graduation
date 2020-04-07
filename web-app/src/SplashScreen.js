ef940bcb8d3b9e982f4ebf22c16b569325f05442
Trevor <trevorzack@gmail.com>
2020-04-07 13:55:07 -0400

    miscellaneous css

--- web-app/src/SplashScreen.js
+++ web-app/src/SplashScreen.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ParamountLogo from "./png/ParamountLogo.png";
+import './SplashScreen.css'
 
 
 const SplashScreen = () => {
     return (
         <header className="App-header">
             <img src={ParamountLogo} className="App-logo" alt="logo"/>
-            <p>
+            <p class="tagg">
                 Future page for Paramount Sports Complex Party Scheduler
             </p>
+            <br/>
             <a
                 className="App-link"
                 href="http://www.paramountsportscomplex.com/"
