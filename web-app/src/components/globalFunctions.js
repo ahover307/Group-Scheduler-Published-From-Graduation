cd387aeb6e1ea7190d88cc0ae322630dfd1f9e41
Alex Hover <ahover307@gmail.com>
2020-04-12 23:10:32 -0400

    Added a function to translate the day of week integer back to string

--- web-app/src/components/globalFunctions.js
+++ web-app/src/components/globalFunctions.js
@@ -62,3 +62,24 @@ export function updatePartyPackageString(e) {
             return "Incorrect Room Code";
     }
 }
+
+export function dayOfWeekIntToString(e) {
+    switch (parseInt(e)) {
+        case 1:
+            return "Sunday";
+        case 2:
+            return "Monday";
+        case 3:
+            return "Tuesday";
+        case 4:
+            return "Wednesday";
+        case 5:
+            return "Thursday";
+        case 6:
+            return "Friday";
+        case 7:
+            return "Saturday";
+        default:
+            return "Unknown day of week"
+    }
+}
