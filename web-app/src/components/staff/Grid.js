dfe5609a9fdf4d65ad14aeedf5d2d5e7bee200c1
molte <molteluca@gmail.com>
2020-04-21 13:52:22 -0400

    Code clean up

--- web-app/src/components/staff/Grid.js
+++ web-app/src/components/staff/Grid.js
@@ -3,6 +3,7 @@ import {Button, Modal, Table} from 'react-materialize'
 import Calendar from "../parentSchedulingProcess/scheduler/Calendar";
 import * as firebase from "firebase";
 import html2pdf from 'html2pdf.js'
+import {translateTimeFromIndexToString, updatePartyPackageString} from '../globalFunctions'
 
 
 const date = new Date();
@@ -29,53 +30,6 @@ class Grid extends Component {
         });
     };
 
-    updatePartyPackageString(e) {
-        switch (parseInt(e)) {
-            case 0:
-                return "Basic";
-            case 1:
-                return "Single";
-            case 2:
-                return "Double";
-            case 3:
-                return "Triple";
-            case 4:
-                return "ERROR";
-            case 5:
-                return "Ninja Exclusive";
-            case 6:
-                return "Ninja Experience";
-            case 7:
-                return "Ninja Extra";
-            case 8:
-                return "Ninja Extreme";
-            case 9:
-                return "Sleepover";
-            default:
-                return "Incorrect Room Code";
-        }
-    }
-
-    translateTimeFromIndexToString(timeIndex) {
-        //I dont trust the input, so I sanitize the input to be an int before manipulating it.
-        timeIndex = parseInt(timeIndex);
-
-        //Find the minutes and hours of each time through the power of math.
-        let hour = (Math.floor(timeIndex / 12) % 12);
-        let minute = ((timeIndex % 12) * 5);
-
-        //Made the offset since it should return a minute hand with a 10s place, even if the tens place is a 0
-        if (minute < 10) {
-            minute = "0" + minute;
-        }
-
-        //Reset to 12 hour time.
-        if (hour === 0) {
-            hour = 12;
-        }
-
-        return (hour + ":" + minute);
-    }
 
     createGrid = () => {
 
@@ -96,6 +50,7 @@ class Grid extends Component {
 
                 }
             });
+
             this.setState({parties: parties});
 
         }).catch(error => {
@@ -107,15 +62,13 @@ class Grid extends Component {
         e.preventDefault();
         const table = document.getElementById('toDownload')
         const opt = {
-            margin: 0.1,
+            margin: 0.15,
             filename: 'grid.pdf',
             image: {type: 'jpeg', quality: 1},
             html2canvas: {scale: 2},
             jsPDF: {unit: 'in', format: 'letter', orientation: 'landscape'}
         };
-// Old monolithic-style usage:
         html2pdf(table, opt);
-
     };
 
 
@@ -171,70 +124,70 @@ class Grid extends Component {
                         </thead>
                         <tbody>
 
-                        {this.state.parties && this.state.parties.map((party, i) => {
+                        {this.state.parties && this.state.parties.sort().map((party, i) => {
                             return (<tr key={i}>
-                                    <td key={i}>{this.translateTimeFromIndexToString(party.data.partyStartTime)} - {this.translateTimeFromIndexToString(party.data.partyEndTime)}
+                                    <td key={i}>{translateTimeFromIndexToString(party.data.partyStartTime)} - {translateTimeFromIndexToString(party.data.partyEndTime)}
                                     </td>
                                     <td key={i + 1}>{party.data.name}</td>
-                                    <td key={i + 2}> {this.updatePartyPackageString(party.data.partyPackage)}</td>
+                                    <td key={i + 2}> {updatePartyPackageString(party.data.partyPackage)}</td>
 
                                     {party.data.roomsRequested[0] === 1 ?
                                         <td key={i + 3}>
-                                            {this.translateTimeFromIndexToString(party.data.roomTimes[0])} - {this.translateTimeFromIndexToString(party.data.roomTimes[1])} </td> :
+                                            {translateTimeFromIndexToString(party.data.roomTimes[0])} - {translateTimeFromIndexToString(party.data.roomTimes[1])} </td> :
                                         party.data.roomsRequested[1] === 1 ?
                                             <td key={i + 3}>
-                                                {this.translateTimeFromIndexToString(party.data.roomTimes[2])} - {this.translateTimeFromIndexToString(party.data.roomTimes[3])} </td> :
+                                                {translateTimeFromIndexToString(party.data.roomTimes[2])} - {translateTimeFromIndexToString(party.data.roomTimes[3])} </td> :
                                             party.data.roomsRequested[2] === 1 ?
                                                 <td key={i + 3}>
-                                                    {this.translateTimeFromIndexToString(party.data.roomTimes[4])} - {this.translateTimeFromIndexToString(party.data.roomTimes[5])} </td> :
+                                                    {translateTimeFromIndexToString(party.data.roomTimes[4])} - {translateTimeFromIndexToString(party.data.roomTimes[5])} </td> :
                                                 <td key={i + 3}/>
                                     }
 
                                     {party.data.roomsRequested[0] === 2 ?
                                         <td key={i + 4}>
-                                            {this.translateTimeFromIndexToString(party.data.roomTimes[0])} - {this.translateTimeFromIndexToString(party.data.roomTimes[1])} </td> :
+                                            {translateTimeFromIndexToString(party.data.roomTimes[0])} - {translateTimeFromIndexToString(party.data.roomTimes[1])} </td> :
                                         party.data.roomsRequested[1] === 2 ?
                                             <td key={i + 4}>
-                                                {this.translateTimeFromIndexToString(party.data.roomTimes[2])} - {this.translateTimeFromIndexToString(party.data.roomTimes[3])} </td> :
+                                                {translateTimeFromIndexToString(party.data.roomTimes[2])} - {translateTimeFromIndexToString(party.data.roomTimes[3])} </td> :
                                             party.data.roomsRequested[2] === 2 ?
                                                 <td key={i + 4}>
-                                                    {this.translateTimeFromIndexToString(party.data.roomTimes[4])} - {this.translateTimeFromIndexToString(party.data.roomTimes[5])} </td> :
+                                                    {translateTimeFromIndexToString(party.data.roomTimes[4])} - {translateTimeFromIndexToString(party.data.roomTimes[5])} </td> :
                                                 <td key={i + 4}/>
                                     }
 
                                     {party.data.roomsRequested[0] === 3 ?
                                         <td key={i + 5}>
-                                            {this.translateTimeFromIndexToString(party.data.roomTimes[0])} - {this.translateTimeFromIndexToString(party.data.roomTimes[1])} </td> :
+                                            {translateTimeFromIndexToString(party.data.roomTimes[0])} - {translateTimeFromIndexToString(party.data.roomTimes[1])} </td> :
                                         party.data.roomsRequested[1] === 3 ?
                                             <td key={i + 5}>
-                                                {this.translateTimeFromIndexToString(party.data.roomTimes[2])} - {this.translateTimeFromIndexToString(party.data.roomTimes[3])} </td> :
+                                                {translateTimeFromIndexToString(party.data.roomTimes[2])} - {translateTimeFromIndexToString(party.data.roomTimes[3])} </td> :
                                             party.data.roomsRequested[2] === 3 ?
                                                 <td key={i + 5}>
-                                                    {this.translateTimeFromIndexToString(party.data.roomTimes[4])} - {this.translateTimeFromIndexToString(party.data.roomTimes[5])} </td> :
+                                                    {translateTimeFromIndexToString(party.data.roomTimes[4])} - {translateTimeFromIndexToString(party.data.roomTimes[5])} </td> :
                                                 <td key={i + 5}/>
                                     }
 
                                     {party.data.roomsRequested[0] === 4 ?
                                         <td key={i + 6}>
-                                            {this.translateTimeFromIndexToString(party.data.roomTimes[0])} - {this.translateTimeFromIndexToString(party.data.roomTimes[1])} </td> :
+                                            {translateTimeFromIndexToString(party.data.roomTimes[0])} - {translateTimeFromIndexToString(party.data.roomTimes[1])} </td> :
                                         party.data.roomsRequested[1] === 4 ?
                                             <td key={i + 6}>
-                                                {this.translateTimeFromIndexToString(party.data.roomTimes[2])} - {this.translateTimeFromIndexToString(party.data.roomTimes[3])} </td> :
+                                                {translateTimeFromIndexToString(party.data.roomTimes[2])} - {translateTimeFromIndexToString(party.data.roomTimes[3])} </td> :
                                             party.data.roomsRequested[2] === 4 ?
                                                 <td key={i + 6}>
-                                                    {this.translateTimeFromIndexToString(party.data.roomTimes[4])} - {this.translateTimeFromIndexToString(party.data.roomTimes[5])} </td> :
+                                                    {translateTimeFromIndexToString(party.data.roomTimes[4])} - {translateTimeFromIndexToString(party.data.roomTimes[5])} </td> :
                                                 <td key={i + 6}/>
                                     }
 
                                     {party.data.roomsRequested[0] === 5 ?
                                         <td key={i + 7}>
-                                            {this.translateTimeFromIndexToString(party.data.roomTimes[0])} - {this.translateTimeFromIndexToString(party.data.roomTimes[1])} </td> :
+                                            {translateTimeFromIndexToString(party.data.roomTimes[0])} - {translateTimeFromIndexToString(party.data.roomTimes[1])} </td> :
                                         party.data.roomsRequested[1] === 5 ?
                                             <td key={i + 7}>
-                                                {this.translateTimeFromIndexToString(party.data.roomTimes[2])} - {this.translateTimeFromIndexToString(party.data.roomTimes[3])} </td> :
+                                                {translateTimeFromIndexToString(party.data.roomTimes[2])} - {translateTimeFromIndexToString(party.data.roomTimes[3])} </td> :
                                             party.data.roomsRequested[2] === 5 ?
                                                 <td key={i + 7}>
-                                                    {this.translateTimeFromIndexToString(party.data.roomTimes[4])} - {this.translateTimeFromIndexToString(party.data.roomTimes[5])} </td> :
+                                                    {translateTimeFromIndexToString(party.data.roomTimes[4])} - {translateTimeFromIndexToString(party.data.roomTimes[5])} </td> :
                                                 <td key={i + 7}/>
                                     }
 
