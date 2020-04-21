8a2d80b7bdf19ae100fa0a8a7d1dc1b606b7a0b8
Trevor <trevorzack@gmail.com>
2020-04-21 14:34:54 -0400

    made app a class again

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component}from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import NavBarComponent from './components/layout/NavBarComponent'
 import Calendar from './components/parentSchedulingProcess/scheduler/Calendar';
@@ -12,14 +12,19 @@ import SearchForm from "./components/staff/SearchForm";
 import Grid from "./components/staff/Grid";
 import PaymentPage from "./components/parentSchedulingProcess/payment/PaymentPage";
 import ParentScheduler from "./components/parentSchedulingProcess/ParentScheduler";
+import SideDrawer from "./components/SideDrawer/SideDrawer";
+import Toolbar from "./components/Toolbar/Toolbar";
+import Backdrop from "./components/Backdrop/Backdrop";
 
 
 //todo submit to the confirmation called
 //todo go to payment before confirming
 
-function App() {
 
-    /*state = {
+
+class App extends Component {
+
+   /* state = {
         sideDrawerOpen: false
     };
 
@@ -33,39 +38,41 @@ function App() {
         this.setState({sideDrawerOpen: false});
     };*/
 
-    /*let backdrop;
+    render() {
 
-    if (this.state.sideDrawerOpen) {
-        backdrop = <Backdrop click={this.backdropClickHandler}/>
-    }*/
-    return (
+        /*let backdrop;
 
-        //Trevor's Nearly Functional Navbar Replacement
-        /*<div style={{height: '100%'}}>
-            <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
-            <SideDrawer show={this.state.sideDrawerOpen}/>
-            {backdrop}
-        </div>*/
+        if (this.state.sideDrawerOpen) {
+            backdrop = <Backdrop click={this.backdropClickHandler}/>
+        }*/
+        return (
+
+            //Trevor's Nearly Functional Navbar Replacement
+            /*<div style={{height: '100%'}}>
+                <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
+                <SideDrawer show={this.state.sideDrawerOpen}/>
+                {backdrop}
+            </div>*/
 
 
-        <div>
-            <BrowserRouter>
-                <NavBarComponent/>
-                <Switch>
-                    <Route exact path='/'> <SplashScreen/> </Route>
-                    <Route path={'/description'}> <PartyDescriptionPage/> </Route>
-                    <Route path={'/scheduler'}> <ParentScheduler/></Route>
-                    <Route path={'/calendar'}> <Calendar/> </Route>
-                    <Route path={'/confirmation'}> <Confirmation/> </Route>
-                    <Route path={'/login'}><SignIn/></Route>
-                    <Route path={'/card'}> <PaymentPage/> </Route>
-                    <Route path={'/dashboard'}><Dashboard/></Route>
-                    <Route path={'/staff/search'}> <SearchForm/></Route>
-                    <Route path={'/staff/grid'}> <Grid/></Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
-    );
+            <div>
+                <BrowserRouter>
+                    <NavBarComponent/>
+                    <Switch>
+                        <Route exact path='/'> <SplashScreen/> </Route>
+                        <Route path={'/description'}> <PartyDescriptionPage/> </Route>
+                        <Route path={'/scheduler'}> <ParentScheduler/></Route>
+                        <Route path={'/calendar'}> <Calendar/> </Route>
+                        <Route path={'/confirmation'}> <Confirmation/> </Route>
+                        <Route path={'/login'}><SignIn/></Route>
+                        <Route path={'/card'}> <PaymentPage/> </Route>
+                        <Route path={'/dashboard'}><Dashboard/></Route>
+                        <Route path={'/staff/search'}> <SearchForm/></Route>
+                        <Route path={'/staff/grid'}> <Grid/></Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
-
 export default App;
