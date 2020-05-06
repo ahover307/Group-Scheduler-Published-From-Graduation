6db755d5d23450747d2f37176bf96f8c8634a15d
Alex Hover <ahover307@gmail.com>
2020-05-06 19:07:24 -0400

    Recreate navbar after luca destroyed that again :)

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,21 +1,17 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import NavBarComponent from './components/layout/NavBarComponent'
-import Calendar from './components/parentSchedulingProcess/scheduler/CalendarComponent';
 import PartyDescriptionPage from "./components/info/PartyDescriptionPage";
-import Confirmation from "./components/parentSchedulingProcess/confirmation/Confirmation";
 import SignIn from "./components/staff/SignIn";
 import Dashboard from "./components/staff/Dashboard";
 import SplashScreen from './SplashScreen';
 import './App.css';
 import SearchForm from "./components/staff/SearchForm";
 import Grid from "./components/staff/Grid";
-import PaymentPage from "./components/parentSchedulingProcess/payment/PaymentPage";
 import ParentScheduler from "./components/parentSchedulingProcess/ParentScheduler";
 import SideDrawer from "./components/SideDrawer/SideDrawer";
 import Toolbar from "./components/Toolbar/Toolbar";
 import Backdrop from "./components/Backdrop/Backdrop";
-import Error from "./components/staff/Error";
 import ReservedTimes from "./components/staff/ReservedTimes";
 
 
@@ -26,7 +22,7 @@ import ReservedTimes from "./components/staff/ReservedTimes";
 class App extends Component {
 
 
-    /* state = {
+     state = {
          sideDrawerOpen: false
      };
 
@@ -37,30 +33,24 @@ class App extends Component {
          });
      };
 
-     backdropClickHandler = () => {
-         this.setState({sideDrawerOpen: false});
-     };*/
+    backdropClickHandler = () => {
+        this.setState({sideDrawerOpen: false});
+    };
 
 
 
     render() {
-
-        /*let backdrop;
+        let backdrop;
 
         if (this.state.sideDrawerOpen) {
             backdrop = <Backdrop click={this.backdropClickHandler}/>
-        }*/
+        }
         return (
-
-            //Trevor's Nearly Functional Navbar Replacement
-            /*<div style={{height: '100%'}}>
+            <div style={{height: '100%'}}>
                 <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
                 <SideDrawer show={this.state.sideDrawerOpen}/>
                 {backdrop}
-            </div>*/
 
-
-            <div>
                 <BrowserRouter>
                     <NavBarComponent/>
                     <Switch>
@@ -68,10 +58,7 @@ class App extends Component {
                         <Route exact path='/'> <SplashScreen/> </Route>
                         <Route path={'/description'}> <PartyDescriptionPage/> </Route>
                         <Route path={'/scheduler'}> <ParentScheduler/></Route>
-                        <Route path={'/calendarComponent'}> <Calendar/> </Route>
-                        <Route path={'/confirmation'}> <Confirmation/> </Route>
                         <Route path={'/login'}><SignIn /></Route>
-                        <Route path={'/card'}> <PaymentPage/> </Route>
                         <Route path={'/staff/dashboard'}> <Dashboard/> </Route>
                         <Route path={'/staff/search'}> <SearchForm/></Route>
                         <Route path={'/staff/grid'}> <Grid/></Route>
